9c4261fc6009b257f23949061b1f83a121558fc7
Hexerse <lucaschuat@hotmail.com>
2024-08-05 20:12:08 +0800

    prep

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const dark = document.querySelector(".dark-mode");
+const light = document.querySelector(".light-mode");
+const icons = document.querySelectorAll(".icon");
+const toggleMode = document.getElementById("toggle-switch");
+const body = document.body;
+
+const darkmode = () => {
+  toggleMode.addEventListener("click", () => {
+    //Body toggle
+    body.classList.toggle("dark-mode");
+    //Icon toggle
+    icons.forEach((icon) => {
+      icon.classList.toggle("dark-switch");
+    });
+  });
+};
+
+window.addEventListener("load", )
+
+
+const main = () => {
+  darkmode();
+};
+
+main();
