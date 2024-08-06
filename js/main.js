d87ffe22afa3fcf908f14803cf872fbe183ce311
Hexerse <lucaschuat@hotmail.com>
2024-08-06 13:19:11 +0800

    Fixed load on dark mode

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ const dark = document.querySelector(".dark-mode");
 const light = document.querySelector(".light-mode");
 const icons = document.querySelectorAll(".icon");
 const toggleMode = document.getElementById("toggle-switch");
-const darkText = document.querySelector('.dark__text')
-const darkBackground = document.querySelector('dark__background')
+const darkText = document.querySelector(".dark__text");
+const darkBackground = document.querySelector("dark__background");
 const body = document.body;
 
 const darkMode = () => {
-  const topicButtons = document.querySelectorAll('.buttons__topics');
-  const quiz = document.querySelector('.frontend__quiz')
+  const topicButtons = document.querySelectorAll(".buttons__topics");
+  const quiz = document.querySelector(".frontend__quiz");
 
   toggleMode.addEventListener("click", () => {
     //Body toggle
     body.classList.toggle("dark-mode");
-    
+
     //Header toggle
     quiz.classList.toggle("dark__text");
 
@@ -23,22 +23,28 @@ const darkMode = () => {
     });
     //toggle topic buttons
     topicButtons.forEach((topic) => {
-      topic.classList.toggle("dark__text")
-      topic.classList.toggle("dark__background")
-    })
-
-
+      topic.classList.toggle("dark__text");
+      topic.classList.toggle("dark__background");
+    });
   });
 };
 
 const mode = () => {
+  const topicButtons = document.querySelectorAll(".buttons__topics");
+  const quiz = document.querySelector(".frontend__quiz");
   if (toggleMode.checked) {
     toggleMode.checked = true;
 
     body.classList.toggle("dark-mode");
+    quiz.classList.toggle("dark__text");
 
     icons.forEach((icon) => {
       icon.classList.toggle("dark-switch");
+    });
+
+    topicButtons.forEach((topic) => {
+      topic.classList.toggle("dark__text");
+      topic.classList.toggle("dark__background");
     });
   }
 };
