5221519d67e08d348114b62b8b16301c59e686b0
Hexerse <lucaschuat@hotmail.com>
2024-08-06 13:10:37 +0800

    header + buttons + dark mode for them

--- js/main.js
+++ js/main.js
@@ -2,16 +2,32 @@ const dark = document.querySelector(".dark-mode");
 const light = document.querySelector(".light-mode");
 const icons = document.querySelectorAll(".icon");
 const toggleMode = document.getElementById("toggle-switch");
+const darkText = document.querySelector('.dark__text')
+const darkBackground = document.querySelector('dark__background')
 const body = document.body;
 
 const darkMode = () => {
+  const topicButtons = document.querySelectorAll('.buttons__topics');
+  const quiz = document.querySelector('.frontend__quiz')
+
   toggleMode.addEventListener("click", () => {
     //Body toggle
     body.classList.toggle("dark-mode");
+    
+    //Header toggle
+    quiz.classList.toggle("dark__text");
+
     //Icon toggle
     icons.forEach((icon) => {
       icon.classList.toggle("dark-switch");
     });
+    //toggle topic buttons
+    topicButtons.forEach((topic) => {
+      topic.classList.toggle("dark__text")
+      topic.classList.toggle("dark__background")
+    })
+
+
   });
 };
 
